774d94e3e92f5e61ecf7929015eadc90cf9e792c
LeeJH0904 <ijaeho813@gmail.com>
2025-05-22 15:49:00 +0900

    Delete PyTest directory

--- PyTest/check_out_service.py
+++
@@ -1,3 +0,0 @@
-class CheckOutService:
-    def create_checkout(self, user_id,book_id):
-        return f"{user_id}_{book_id}_checkout"
--- PyTest/login_service.py
+++
@@ -1,4 +0,0 @@
-class LoginService:
-    def accountCheck(self, user_id, password):
-        if (user_id == "user1" and password == "password123"):
-            return True
